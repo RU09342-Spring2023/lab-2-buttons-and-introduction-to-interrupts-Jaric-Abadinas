e678ba87c959ab994e47c2838e2ecf932e25b105
Jaric <abadin48@students.rowan.edu>
2023-02-07 12:54:43 -0500

    Part 4 - Started designing occupancy detector
    Added example, name, and linked to gitkraken

--- 4 Designing an Intruder Security System/OccupancyDetector.c
+++ 4 Designing an Intruder Security System/OccupancyDetector.c
@@ -2,9 +2,34 @@
  * OccupancyDetector.c
  *
  *  Created on: Jan 30, 2023
- *      Author: russty
+ *      Author: Jaric Abadinas
  */
 
+#include <msp430.h>
 
+#define ARMED_STATE 0
+#define WARNING_STATE 1
+#define ALERT_STATE 2
 
+// Put some initialization here
+
+    char state = ARMED_STATE;
+
+int main() {
+    while(1)
+    {
+      switch (state) {
+        case ARMED_STATE:
+        {
+          // Do something in the ARMED state
+          // If something happens, you can move into the WARNING_STATE
+          // state = WARNING_STATE
+        }
+        case WARNING_STATE:
+        {
+          // Do something in the WARNING_STATE
+        }
+      }
+    }
+}
 
